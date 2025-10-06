ae943cb0404f80d926ae60a35f5984a37ffabbe9
dobosevych <dobosevych@gmail.com>
2025-10-06 10:43:53 +0300

    first commit

--- src/main/java/ucu/edu/ua/apps/Main.java
+++ src/main/java/ucu/edu/ua/apps/Main.java
@@ -1,9 +1,11 @@
 package ucu.edu.ua.apps;
 
+import java.util.concurrent.Flow;
+
 public class Main {
     public static void main(String[] args) {
         FlowerColor flowerColor = FlowerColor.RED;
-        System.out.println(flowerColor); // "#FF0000"
- 
+        System.out.println(flowerColor); // "#FF0000"    
+        Flower flower = new Flower(100, FlowerColor.RED, 100, FlowerType.ROSE);    
     }
 }
